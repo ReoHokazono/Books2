b738d5afc42b1c85ca7258d066cb41a7dd26e6c0
hokazono <reohokazono@gmail.com>
2020-10-27 00:51:00 +0900

    Initial Commit

---
+++ Books2/Books2App.swift
@@ -0,0 +1,20 @@
+//
+//  Books2App.swift
+//  Books2
+//
+//  Created by 外園玲央 on 2020/10/27.
+//
+
+import SwiftUI
+
+@main
+struct Books2App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
